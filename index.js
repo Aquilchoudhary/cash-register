7bed075dcdfbc6d7ecd679bc3f6f92a18e5ae1c2
Aquilchoudhary <90390008+Aquilchoudhary@users.noreply.github.com>
2021-09-29 23:48:27 +0530

    update kogic

---
+++ index.js
@@ -0,0 +1,35 @@
+const billAmount=document.querySelector("#bill-amount");
+const cashGiven=document.querySelector("#cash-given");
+const checkBtn=document.querySelector("#check-button");
+const outputBox=document.querySelector("#output");
+const availableNotes=[2000, 500, 100, 20, 10, 5, 1];
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+function calculateChange(AmountToBeReturned){
+    for(let i=0;i < availableNotes.length; i++){
+        const numberOfNotes= Math.trunc(AmountToBeReturned/availableNotes[i]);
+        AmountToBeReturned=AmountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText=numberOfNotes;
+
+
+    }
+    
+}
+
+function validateBillAndCashAmount() {
+    if(billAmount.value > 0){
+        if(cashGiven.value > billAmount.value){
+            const AmountToBeReturned=cashGiven.value-billAmount.value;
+            calculateChange(AmountToBeReturned);
+
+        }else{
+            outputBox.innerText="Do you wanna wash plates ? "
+        }
+    }
+    else{
+        outputBox.innerText="The Bill Amount should be greater than zero";
+       
+    }
+}
+
+checkBtn.addEventListener("click",validateBillAndCashAmount);
